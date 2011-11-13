b8e45a4f145eb0214bf5a4c576b0a8cab298d6c7
Georgi Hristozov <georgi@forkbomb.nl>
2011-11-13 23:38:56 +0200

    Fixing the first solution.

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,15 +1,24 @@
 class Array
 
   def to_hash
-    self.inject({}) {|result, item| result[item[0]] = item[1]; result}
+    self.inject({}) do |result, item|
+      result[item[0]] = item[1]
+      result
+    end
   end
 
   def index_by
-    self.inject({}) {|result, item| result[yield item] = item; result}
+    self.inject({}) do
+      |result, item| result[yield item] = item
+      result
+    end
   end
 
   def occurences_count
-    self.inject(Hash.new(0)) {|result, item| result[item] += 1; result }
+    self.inject(Hash.new(0)) do |result, item|
+      result[item] += 1
+      result
+    end
   end
 
   def subarray_count(subarray)
